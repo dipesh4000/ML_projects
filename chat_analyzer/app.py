baf14e4d2468e4a458330490d744d0afa830b92f
Dipesh Kumar <dipeshkumar0853822@gmail.com>
2026-01-19 11:20:39 +0530

    figure representation of different type of activities facing some issue with the dataframe

--- chat_analyzer/app.py
+++ chat_analyzer/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import preprocessor
 import helper
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.sidebar.title('Whatsapp chat Analyzer')
 
@@ -22,6 +23,7 @@ if uploaded_file is not None:
 
     if st.sidebar.button("Analyze"):
         num_messages, num_words, num_media_messages, num_links  = helper.fetch_stats(selected_user, df)
+        st.title("Top Statistics")
         col1, col2, col3, col4 = st.columns(4)
 
         with col1:
@@ -36,6 +38,48 @@ if uploaded_file is not None:
         with col4:
             st.header("Total Links")
             st.title(num_links)
+
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'], color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # daily timeline
+        st.title("Daily Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # activity map
+        st.title('Activity Map')
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header("Most busy day")
+            busy_day = helper.week_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values, color='purple')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        with col2:
+            st.header("Most busy month")
+            busy_month = helper.month_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values, color='orange')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        st.title("Weekly Activity Map")
+        user_heatmap = helper.activity_heatmap(selected_user, df)
+        fig, ax = plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
+
         if selected_user == 'Overall':
             st.title("Most busy users")
             x, new_df = helper.most_busy_users(df)
--- chat_analyzer/helper.py
+++ chat_analyzer/helper.py
@@ -1,6 +1,7 @@
 from urlextract import URLExtract
 import pandas as pd
 from collections import Counter
+import re
 
 extractor = URLExtract()
 def fetch_stats(selected_user, df):
@@ -38,13 +39,59 @@ def most_common_words(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
 
-    temp = df.query('message != "<Media omitted>" and message != "<This message was edited>"')
-    temp = temp.reset_index(drop=True)
+    df['message'] = df['message'].apply(lambda x: re.sub(r'<.*?>', '', x))
+    df = df.reset_index(drop=True)
 
     words = []
-    for message in temp['message']:
+    for message in df['message']:
         for word in message.lower().split():
             if word not in stop_words:
                 words.append(word)
     most_common_df = pd.DataFrame(Counter(words).most_common(20))
     return most_common_df
+def monthly_timeline(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    timeline = df.groupby(['year', 'month']).count()['message'].reset_index()
+
+    time = []
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+
+    timeline['time'] = time
+
+    return timeline
+
+def daily_timeline(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    daily_timeline = df.groupby('only_date').count()['message'].reset_index()
+
+    return daily_timeline
+
+def week_activity_map(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    return df['day_name'].value_counts()
+
+def month_activity_map(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    return df['month'].value_counts()
+
+def activity_heatmap(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    user_heatmap = df.pivot_table(index='day_name', columns='period', values='message', aggfunc='count').fillna(0)
+
+    return user_heatmap
